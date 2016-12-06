4f2e7bf36e9baedb22ef65289801b7cc18b421f5
heweiming <heweiming89@163.com>
2016-12-06 22:42:48 +0800

    增加 common

--- freedom-service/src/main/java/cn/heweiming/freedom/service/BaseService.java
+++ freedom-service/src/main/java/cn/heweiming/freedom/service/BaseService.java
@@ -1,13 +1,13 @@
 package cn.heweiming.freedom.service;
 
+import org.activiti.engine.FormService;
 import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.ManagementService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
 
 //@Lazy
 public abstract class BaseService {
@@ -26,4 +26,12 @@ public abstract class BaseService {
 
     @Autowired
     protected ManagementService managementService;
+    
+    @Autowired
+    protected FormService formService;
+    
+    @Autowired
+    protected IdentityService identityService;
+    
+    
 }
--- freedom-test/src/test/java/cn/heweiming/freedom/test/activiti/ActivitiTest01.java
+++ freedom-test/src/test/java/cn/heweiming/freedom/test/activiti/ActivitiTest01.java
@@ -1,10 +1,16 @@
 package cn.heweiming.freedom.test.activiti;
 
+import java.util.List;
+import java.util.Map;
+
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.ManagementService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +18,53 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:applicationContext.xml", "classpath*:includes/applicationContext-activiti.xml" })
+@ContextConfiguration(locations = { "classpath*:applicationContext.xml",
+		"classpath*:includes/applicationContext-activiti.xml" })
 public class ActivitiTest01 {
 
-    @Autowired
-    private RepositoryService repositoryService;
-    
-    @Autowired
-    private RuntimeService runtimeService;
-    
-    @Autowired
-    private TaskService taskService;
-    
-    @Autowired
-    private HistoryService historyService;
-    
-    @Autowired
-    private ManagementService managementService;
-    
-    
-    @Test
-    public void test() {
-        System.out.println(11);
-    }
+	@Autowired
+	private RepositoryService repositoryService;
+
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private TaskService taskService;
+
+	@Autowired
+	private HistoryService historyService;
+
+	@Autowired
+	private ManagementService managementService;
+
+	@Test
+	@Ignore
+	public void deployment() {
+		Deployment deployment = repositoryService.createDeployment() //
+				.addClasspathResource("helloworld.bpmn").name("helloworld").deploy();
+		deployment.getId();
+		long count = repositoryService.createProcessDefinitionQuery().count();
+		System.out.println(deployment.getId());
+		System.out.println(count);
+	}
+
+	@Test
+	public void test() {
+		List<Deployment> list = repositoryService.createDeploymentQuery().list();
+		for (Deployment deployment : list) {
+			System.out.println(deployment);
+		}
+		System.out.println("----------------");
+		List<ProcessDefinition> list2 = repositoryService.createProcessDefinitionQuery().list();
+		for (ProcessDefinition processDefinition : list2) {
+			System.out.println(processDefinition);
+		}
+	}
+
+	@Test
+	public void startProcess() throws Exception {
+		Map<String, Object> variables = null;
+		runtimeService.startProcessInstanceByKey("myProcess", variables);
+	}
 
 }
